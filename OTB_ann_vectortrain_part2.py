d4bf6001055c093de298b32dd54f48587aa07454
RTS <25514511+realtechsupport@users.noreply.github.com>
2021-12-04 15:07:05 -0500

    integrate input file

--- OTB_ann_vectortrain_part2.py
+++ OTB_ann_vectortrain_part2.py
@@ -3,8 +3,9 @@
 # install on Ubuntu 18 LTS with conda (conda-packages1.sh and environmentv1.yml.)
 # RTS, Nov 2021
 # OTB_part2
+# reference: https://www.orfeo-toolbox.org/CookBook-6.2/Applications/app_TrainVectorClassifier.html
 
-#sequence
+# sequence
 # OTB_part1
 # QGIS_join
 # OTB_part2
@@ -30,7 +31,7 @@ try:
 except:
         print('\n...data access error...\n')
 else:
-	print(jdata)
+	#print(jdata)
 	rasterimage = jdata['rasterimage']
 	pointsfile = jdata['pointsfile']
 	classifier_type = jdata['classifier']
@@ -45,34 +46,49 @@ else:
 	modelname = jdata['modelname']
 	confusion_matrix = jdata['confusion_matrix']
 	ann_vector_classification = jdata['ann_vector_classification']
+	cfieldname = jdata['cfieldname']
+	NNtraintype = jdata['NNtraintype']
+	NNsize = jdata['NNsize']	#10 or 10, 10 (one or two hidden layers)
+	NNactivation = jdata['NNactivation']
+	NNalpha = float(jdata['NNalpha'])
+	NNbeta = float(jdata['NNbeta'])
+	NNbackpropweightgradient = float(jdata['NNbackpropweightgradient'])
+	NNmomentum = float(jdata['NNmomentum'])
+	NNinitialdelta = float(jdata['NNinitialdelta'])
+	NNupdatedelta = float(jdata['NNupdatedelta'])
+	NNterminationcriteria = jdata['NNterminationcriteria']
+	NNepsilon = float(jdata['NNepsilon'])
+	NNmaxiterations = int(jdata['NNmaxiterations'])
+#------------------------------------------------------------------------------
 
 if(classifier_type == 'ann'):
 	#TrainVectorClassifiers
 	sfile = vectorpath + segmentation_points_joined
 	app = otbApplication.Registry.CreateApplication("TrainVectorClassifier")
 
+	mean_std_list = ["mean_0", "stdev_0", "mean_1", "stdev_1", "mean_2", "stdev_2", "mean_3", "stdev_3"]
 	print('\n\n Training Neural Network Model\n\n')
 	app.SetParameterStringList("io.vd", [sfile])
 	app.SetParameterString("io.out", resultspath + modelname)
-	
-	app.SetParameterStringList("feat", ["mean_0", "stdev_0", "mean_1", "stdev_1", "mean_2", "stdev_2", "mean_3", "stdev_3"])
-	app.SetParameterInt("valid.layer", 0) 
-	app.SetParameterString("cfield", "class")
+	#--------------------
+	app.SetParameterStringList("feat", mean_std_list)
+	app.SetParameterInt("valid.layer", 0)
 
-	app.SetParameterString("classifier.ann.t", "reg") #reg, back
-	app.SetParameterStringList("classifier.ann.sizes", ["10"])
-	app.SetParameterString("classifier.ann.f", "sig") #ident, sig, gau
-	app.SetParameterFloat("classifier.ann.a", 1.0)
-	app.SetParameterFloat("classifier.ann.b", 1.0)
-	app.SetParameterFloat("classifier.ann.bpdw", 0.1)
-	app.SetParameterFloat("classifier.ann.bpms", 0.1)
-	app.SetParameterFloat("classifier.ann.rdw", 0.1)
-	app.SetParameterFloat("classifier.ann.rdwm", 1e-07)
-	app.SetParameterString("classifier.ann.term", "all")  #iter, eps, all
-	app.SetParameterFloat("classifier.ann.eps", 0.01)
-	app.SetParameterInt("classifier.ann.iter", 1000)
+	app.SetParameterString("cfield", cfieldname)
+	app.SetParameterString("classifier.ann.t", NNtraintype) 		#reg, back
+	app.SetParameterStringList("classifier.ann.sizes", [NNsize])		#10 or 10, 10 (one or two hidden layers)
+	app.SetParameterString("classifier.ann.f", NNactivation) 		#ident, sig, gau
+	app.SetParameterFloat("classifier.ann.a", NNalpha)
+	app.SetParameterFloat("classifier.ann.b", NNbeta)
+	app.SetParameterFloat("classifier.ann.bpdw", NNbackpropweightgradient)
+	app.SetParameterFloat("classifier.ann.bpms", NNmomentum)
+	app.SetParameterFloat("classifier.ann.rdw", NNinitialdelta)
+	app.SetParameterFloat("classifier.ann.rdwm", NNupdatedelta)
+	app.SetParameterString("classifier.ann.term", NNterminationcriteria)	#iter, eps, all
+	app.SetParameterFloat("classifier.ann.eps", NNepsilon)
+	app.SetParameterInt("classifier.ann.iter", NNmaxiterations)
+	#--------------------
 	app.SetParameterInt("rand", 0)
-
 	app.SetParameterString("io.confmatout", resultspath + confusion_matrix)
 	app.ExecuteAndWriteOutput()
 	print('\n\nvector train complete... \n\n')
@@ -85,7 +101,7 @@ if(classifier_type == 'ann'):
 	app = otbApplication.Registry.CreateApplication(apptype)
 	app.SetParameterString("in", vectorpath + inputshapefile)
 	app.SetParameterString("model", resultspath + modelname)
-	app.SetParameterStringList("feat", ["mean_0", "stdev_0", "mean_1", "stdev_1", "mean_2", "stdev_2", "mean_3", "stdev_3"])
+	app.SetParameterStringList("feat", mean_std_list)
 	app.SetParameterString("out", resultspath + sfileout)
 	app.ExecuteAndWriteOutput()
 	print('\n\nvector classify complete... \n\n')

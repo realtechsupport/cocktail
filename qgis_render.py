fcebd24b246bf9e842efff3669023c99f3075b8e
RTS <25514511+realtechsupport@users.noreply.github.com>
2021-12-06 20:25:25 -0500

    include dissolve

--- qgis_render.py
+++ qgis_render.py
@@ -1,12 +1,16 @@
 # qgis_remder.py
-# RTS, November 2021
-# render vector classification with a .qml color map
+# RTS, December 2021
+# select dissolve and then render vector classification with a .qml color map
 #---------------------------------------------------------------------------------------
 #sequence
 # OTB_part1
 # QGIS_join
 # OTB_part2
 # QGIS_render
+
+import sys
+#clear all variables (from last session)
+#sys.modules[__name__].__dict__.clear()
 
 import os, sys, json
 from datetime import datetime
@@ -57,42 +61,55 @@ else:
 	ann_vector_classification = jdata['ann_vector_classification']
 	colortemplate = jdata['colortemplate']
 	classified_vimage = jdata['classified_vimage']
+
+	perform_dissolve = jdata['perform_dissolve']
+	dissolved_shapefile = jdata['dissolved_shapefile']
+	compute_area = jdata['COMPUTE_AREA']
+	compute_statistics = jdata['COMPUTE_STATISTICS']
+	count_features = jdata['COUNT_FEATURES']
+	explode_collections = jdata['EXPLODE_COLLECTIONS']
+	ref_field = jdata['FIELD']
+	geometry = jdata['GEOMETRY']
+	keep_attributes = jdata['KEEP_ATTRIBUTES']
+	options = jdata['OPTIONS']
+	statistics_attribute = jdata['STATISTICS_ATTRIBUTE']
+	
+	t2p = jdata['T2P']
 	pdir = jdata['pdir']
 	r_height = int(jdata['r_height'])
 	r_width = int(jdata['r_width'])
 	background = jdata['background']
 
+
 #output from OTB_part2
-shapefile = resultspath + ann_vector_classification
+render_shapefile = resultspath + ann_vector_classification
 
-#add timestamp to classified vimage
+#create the final image name
 rastername = rasterimage.split('.tif')[0]
-classified_vimage = rastername + '_' + classified_vimage + '_' + classifier + '.png'
-
-T2P = True
 
 #-------------------------------------------------------------------------------
 #qgis core module imports
 from qgis.core import (
-    QgsApplication, 
-    QgsPrintLayout,
-    QgsMapSettings,
-    QgsMapRendererParallelJob,
-    QgsProcessingFeedback, 
-    QgsVectorLayer,
-    QgsRasterLayer,
-    QgsField,
-    QgsFields,
-    QgsProject,
-    QgsProperty,
-    QgsProcessingFeatureSourceDefinition,
-    QgsProcessingOutputLayerDefinition
+	QgsApplication, 
+	QgsPrintLayout,
+	QgsMapSettings,
+	QgsMapRendererParallelJob,
+	QgsProcessingFeedback, 
+	QgsProcessingException,
+	QgsProcessingParameters,
+	QgsVectorLayer,
+	QgsRasterLayer,
+	QgsField,
+	QgsFields,
+	QgsProcessingFeatureSourceDefinition,
+	QgsProcessingOutputLayerDefinition,
+	QgsProject,
+	QgsProperty
 )
-
 from qgis.PyQt.QtCore import (
-    QPointF,
-    QRectF,
-    QSize,
+	QPointF,
+	QRectF,
+	QSize,
 )
 #------------------------------------------------------------------------------
 #start Qgis
@@ -107,13 +124,49 @@ from processing.core.Processing import Processing
 processing.core.Processing.Processing.initialize()
 from processing.tools import *
 
-#write the features of the vector layer to a file
-vlayer = QgsVectorLayer(shapefile, "temp", "ogr")
+if(perform_dissolve == "yes"):
+	#dissolve the layer ----------------------------------------------------------------------
+	classified_vimage = rastername + '_' + classified_vimage + '_' + classifier + '_dissolved.png'
+	dissolved_shapefile =  resultspath + dissolved_shapefile
+	algorithmname = "gdal:dissolve"
+	print('\n\nRunning the Gdal dissolve now...this step takes about 20 minutes [16GB RAM]\n')
 
+	parameters = {
+		'INPUT': render_shapefile,
+		'COMPUTE_AREA' : compute_area,
+		'COMPUTE_STATISTICS' : compute_statistics,
+		'COUNT_FEATURES' : count_features,
+		'EXPLODE_COLLECTIONS' : explode_collections,
+		'FIELD' : ref_field, 
+		'GEOMETRY' : geometry,
+		'KEEP_ATTRIBUTES' : keep_attributes,
+		'OPTIONS' : options,
+		'STATISTICS_ATTRIBUTE' : statistics_attribute,
+		'OUTPUT' : dissolved_shapefile}
+
+	feedback = QgsProcessingFeedback()
+
+	try:
+		results = processing.run(algorithmname, parameters, feedback=feedback)
+		working_shapefile = dissolved_shapefile
+		print('...completed: ', algorithmname)
+
+	except QgsProcessingException as e:
+		print('\n\n ERROR in this operation..')
+		print(str(e))
+
+else:
+	print('\n\nNot performing the dissolve operation...')
+	classified_vimage = rastername + '_' + classified_vimage + '_' + classifier + '.png'
+	working_shapefile = render_shapefile
+
+#--------------------------------------------------------------------------------------
+#write the features of the vector layer to a file
+vlayer = QgsVectorLayer(working_shapefile, "temp", "ogr")
 if not vlayer.isValid():
 	print("Vector data failed to load!\n")
 else:
-	print('vector data loaded...\n')
+	print("Vector data loaded...\n")
 
 #add color template
 vlayer.loadNamedStyle(datapath + colortemplate)
@@ -146,13 +199,13 @@ qgs.exitQgis()
 
 #-----------------------------------------------------------
 
-if(T2P):
+if(t2p == "yes"):
 	f = open(authfile, 'r')
 	lines = f.readlines()
 	username = lines[0].strip()
 	password = lines[1].strip()
 	f.close()
-	try:	
+	try:
 		conn = PyCloud(username, password, endpoint='nearest')
 		filelist = [resultspath + classified_vimage]
 		conn.uploadfile(files=filelist, path=pdir)

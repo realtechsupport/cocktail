6f2ff0a45964f249dec02b67fb327137979947bb
RTS <25514511+realtechsupport@users.noreply.github.com>
2022-03-05 19:40:17 -0500

    updates

--- code/otb_vary_raster_classify.py
+++ code/otb_vary_raster_classify.py
@@ -5,94 +5,99 @@
 # get token names and defaul values from the settings file.
 # enable the OTB environment before you run this (conda activate OTB)
 # RTS, march 2022
-#----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 import os, sys
 import shutil, time
 import subprocess
 from helper import *
-#----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 datapath = '/home/blc/cocktail/data/'
 codepath = '/home/blc/cocktail/code/'
 settingsname = "settings.txt"
 orgfile = "settings_org.txt"
 infile = settingsname
 outfile = infile
-
+#-------------------------------------------------------------------------------
 # set the the classifier to call
 program = 'otb_raster_classify.py'
-choice  =  'SVM'
+selection  =  'libsvm' #or rf
 
-#-----------------------------------------------------------------------------
-if (choice == 'SVM'):
-	selection = 'libsvm'
-	#parameter names to vary
-	token1_name = "svm_k"
-	token2_name = "svm_c"
-	#default values
-	token1_value = "linear"
-	token2_value = "1"
+#-------------------------------------------------------------------------------
+def main():
+	# print command line arguments
+	print('\nRunning this classifier with parameter permutations: ', selection)
+	vary_raster_classify(datapath, codepath, infile, orgfile, outfile, selection)
+	print('\nFinished parameter permutations\n')
 
-	#leave unchanged ... token1_list = ["linear"]
-	#OR multiple values [a,b,c]
+#-------------------------------------------------------------------------------
+def vary_raster_classify(datapath, codepath, infile, orgfile, outfile, selection):
+	if (selection == 'libsvm'):
+		#parameter names to vary
+		token1_name = "svm_k"
+		token2_name = "svm_c"
+		#default values
+		token1_value = "linear"
+		token2_value = "1"
 
-	#SVM: linear/rbf/poly/sigmoid
-	token1_list = ['rbf', 'poly']
-	#0.1 ... 1.0
-	token2_list = ["0.7", "0.4"]
+		#leave unchanged ... token1_list = ["linear"]
+		#OR multiple values [a,b,c]
 
-elif (choice == 'RF'):
-	#"rf_max" : "5", "rf_min" : "1",
-	#"rf_ra" : "0", "rf_cat" : "10",
-	#"rf_var" : "0", "rf_ntrees" : "100",
-	#"rf_acc" : "0.01"
+		#SVM: linear/rbf/poly/sigmoid
+		token1_list = ['rbf', 'poly']
+		#0.1 ... 1.0
+		token2_list = ["0.7", "0.4"]
 
-	selection =  'rf'
-	token1_name = 'rf_ntrees'
-	token2_name = 'rf_min'
-	token1_value = '100'		#get these from the current setting file
-	token2_value = '1'
-	token1_list = ['300', '1000']
-	token2_list = ['3', '5']
+	elif (selection == 'rf'):
+		#"rf_max" : "5", "rf_min" : "1",
+		#"rf_ra" : "0", "rf_cat" : "10",
+		#"rf_var" : "0", "rf_ntrees" : "100",
+		#"rf_acc" : "0.01"
+		token1_name = 'rf_ntrees'
+		token2_name = 'rf_min'
+		token1_value = '100'		#get these from the current setting file
+		token2_value = '1'
+		token1_list = ['300', '1000']
+		token2_list = ['3', '5']
 
-else:
-	print('Check the choice...')
-	sys.exit()
+	else:
+		print('ERROR...Check the selected classifier or the parameters...')
+		sys.exit()
 
-#-----------------------------------------------------------------------------
+	#make backup of settings file
+	shutil.copyfile(datapath+infile, datapath+orgfile)
 
-#make backup of settings file
-shutil.copyfile(datapath+infile, datapath+orgfile)
+	#run classifier with default settings.
+	try:
+		print('\n\nRunning classifier with DEFAULT settings.\n\n')
+		command = 'python3 ' + codepath + program + ' ' + selection
+		os.system(command)
+	except:
+		print('\nError running classifier...')
 
-#run classifier with default settings.
-try:
-	print('\n\nrunning classifier with settings..DEFAULT\n\n')
-	command = 'python3 ' + codepath + program + ' ' + selection
-	os.system(command)
-except:
-	print('\nError running classifier...')
+	#make change based on list of token1_list, token2_list and run classifier after each change
+	for i in range (0, len(token1_list)):
+		for j in range (0, len(token2_list)):
+			change1 = token1_list[i]
+			change2 = token2_list[j]
+			change_settings(datapath, infile, token1_name, token1_value, token2_name, token2_value, change1, change2, outfile)
+			#use the origininal settings as reference again
+			infile = orgfile
+			try:
+				print('\n\nRunning classifier with settings..', i, j, change1, change2)
+				command = 'python3 ' + codepath + program + ' ' + selection
+				os.system(command)
+				#wait one minute
+				print('\nWaiting for 2 minutes...\n')
+				time.sleep(120)
+			except:
+				print('\nError running classifier...')
+				break
 
-#make change based on list of token1_list, token2_list and run classifier after each change
-for i in range (0, len(token1_list)):
-	for j in range (0, len(token2_list)):
-		change1 = token1_list[i]
-		change2 = token2_list[j]
-		change_settings(datapath, infile, token1_name, token1_value, token2_name, token2_value, change1, change2, outfile)
-		#use the origininal settings as reference again
-		infile = orgfile
-		try:
-			print('\n\nrunning classifier with settings..', i,j, change1, change2)
-			command = 'python3 ' + codepath + program + ' ' + selection
-			os.system(command)
-			#wait one minute
-			print('\n\n Waiting for 2 minutes...')
-			time.sleep(120)
-		except:
-			print('\nError running classifier...')
-			break
+	print("RESETTING input file to start position")
+	os.remove(datapath+outfile)
+	os.rename(datapath+orgfile, datapath+settingsname)
 
-print("RESETTING input file to start position")
-os.remove(datapath+outfile)
-os.rename(datapath+orgfile, datapath+settingsname)
-
-#---------------------------------------------------------------------------------
-
+#-------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
+#-------------------------------------------------------------------------------

d18fe524154a6361ad76167c608b3f7307b22a3d
RTS <25514511+realtechsupport@users.noreply.github.com>
2022-04-28 17:06:29 -0400

    Add files via upload

--- code/sentinel2_getdata.py
+++ code/sentinel2_getdata.py
@@ -28,17 +28,20 @@ from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
 from datetime import date
 
 from helper import *
+from sentinel2_helper import *
 
 # Local path and variables
 datapath = '/home/blc/cocktail/data/'
 inputsfile = datapath + 'settings.txt'
 
 #-------------------------------------------------------------------------------
+
 def main():
 	print("\nProceeding with data collection")
 	get_sentinel_data()
 
 #--------------------------------------------------------------------------------
+
 def get_sentinel_data():
 	try:
         	f = open(inputsfile, 'r')
@@ -50,6 +53,8 @@ def get_sentinel_data():
 	else:
 		sentinelrasterpath = jdata['sentinelrasterpath']
 		landsat8rasterpath = jdata['landsat8rasterpath']
+		vectorpath = jdata['vectorpath']
+		collectionpath = jdata['collectionpath']
 		rawsatpath = jdata['rawsatpath']
 		authfile = jdata['authfile']
 		sentinelauthfile = jdata['sentinelauthfile']
@@ -112,6 +117,52 @@ def get_sentinel_data():
 		print (message)
 		print('\n... Something went wrong ... see below...')
 		print('possible error sources: geojson file, ndays, nmonths and cloud cover setting ...')
+
+
+	# unpack the package if it has been received
+	if(len(products_df_sorted) > 0):
+		unpack(rawsatpath)
+
+	tci_path = get_tci_path(rawsatpath)
+	jp2_list = [band for band in os.listdir(tci_path) if band [-4:] == '.jp2']
+
+	# two versions here: 1: clip and make thubmnail from the TCI only
+	# 2. convert all bands, clip, zip and move to collection
+
+	#convert .jp2 to .tif
+	print('\nconverting .jp2 to .tif\n')
+	tci_path = tci_path + '/'
+	for image in jp2_list:
+		timage = image.split('.jp2')[0] + '.tif'
+		ds = gdal.Translate(tci_path + timage, tci_path + image)
+		ds = None
+
+	tif_list =[band for band in os.listdir(tci_path) if band[-4:] == '.tif']
+	print(tif_list) 
+
+	# move and unpack the shapefile to the ROI dir in the vectorpath
+	roishapezipfile =  'area2_shape_crop.zip'
+	roishape = roishapezipfile.split('.zip')[0] + '.shp'
+	roipath = vectorpath + 'roi/'
+	roipath_area2crop = roipath + roishapezipfile.split('.zip')[0] + '/'
+
+	# eigentlich only have to unzip the ROI file once..
+	shutil.copy(collectionpath + roishapezipfile, roipath)
+	with zipfile.ZipFile(roipath + roishapezipfile, 'r') as zip_ref:
+		zip_ref.extractall(roipath)
+
+	#clip bands
+	warp_options = gdal.WarpOptions(cutlineDSName = roipath_area2crop+roishape, cropToCutline = True)
+
+	for b in tif_list:
+		b_new = b.split('.tif')[0] + '_roi' + '.tif'
+		print(b, b_new)
+		ds = gdal.Warp(tci_path + b_new, tci_path + b, options = warp_options)
+		ds = None
+
+	# got this far ----------------------------------------------------------
+	# zip up and move to collection, then delete everything in rawsat directory
+	# https://stackoverflow.com/questions/1855095/how-to-create-a-zip-archive-of-a-directory 
 
 #--------------------------------------------------------------------------------
 
